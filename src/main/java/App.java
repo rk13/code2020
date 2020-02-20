3008a2d0887ad6280d577460395f3bfa2fec3e1e
Vladimirs Kotovs <vladimirs.kotovs@klarna.com>
2020-02-20 20:47:29 +0100

    :)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,15 +8,18 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
+
         try {
             Scanner scanner = new Scanner(new File("b_read_on.txt"));
 
             String[] firstLine = scanner.nextLine().split(" ");
+
             Integer B = Integer.parseInt(firstLine[0]);
             Integer L = Integer.parseInt(firstLine[1]);
             Integer D = Integer.parseInt(firstLine[2]);
 
             Map<Integer, Integer> bookscores = new HashMap<>();
+
             String[] scoreLineString = scanner.nextLine().split(" ");
             for (int i = 0; i < scoreLineString.length; i++) {
                 bookscores.put(i, Integer.parseInt(scoreLineString[i]));
@@ -24,6 +27,7 @@ public class App {
 
             Map<Integer, Map<String, Integer>> libraryInfoMap = new HashMap<>();
             Map<Integer, List<Integer>> libraryBooksMap = new HashMap<>();
+
             for (int j = 0; j < L; j++) {
                 libraryInfoMap.put(j, new HashMap<>());
                 libraryBooksMap.put(j, new ArrayList<>());
@@ -39,17 +43,19 @@ public class App {
                 }
             }
 
-            System.out.println(B);
-            System.out.println(L);
-            System.out.println(D);
-            System.out.println(bookscores);
-            System.out.println(libraryInfoMap);
-            System.out.println(libraryBooksMap);
+            // :)
+            System.out.print(1);
+            System.out.print(' ');
+            System.out.println(libraryBooksMap.get(0).size());
+            for(Integer i : libraryBooksMap.get(0).subList(0, 1000)) {
+                System.out.print(i);
+                System.out.print(' ');
+            }
+
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
-
 }
