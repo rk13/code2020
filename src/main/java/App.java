f800509b3b06bcf4dbeaa416e162603f2c56c2e3
Vladimirs Kotovs <vladimirs.kotovs@klarna.com>
2020-02-20 17:03:40 +0100

    Parsing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("!");
+    static String[][] input;
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("/tmp/input.txt");
+        Scanner in = new Scanner(new BufferedReader(new FileReader(file)));
+        int t = Integer.parseInt(in.nextLine());
+
+        input = new String[t][];
+        for (int i = 1; i <= t; i++) {
+            input[i - 1] = in.nextLine().split(" ");
+        }
+
     }
 }
