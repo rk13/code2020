fa83d50549998f7f323fd9e325b8af2d99bc22d9
Boshra Taheri <boshra.taheri@klarna.com>
2020-02-20 20:40:55 +0100

    Change score calculation in Library class

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,4 +1,3 @@
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -14,19 +13,11 @@ public class Library {
         bookList.add(book);
     }
 
-    public int calculateScore(int deadLine, Map<Integer, Book> bookStore) {
+    public List<Book> getBestBooks(int deadLine, Map<Integer, Book> bookStore) {
         int scanBeforeDeadline = (deadLine - signUp) * booksPerDay;
         List<Book> unscannedBooks = bookList.stream()
                 .filter(i -> !bookStore.get(i.index).isScanned).collect(Collectors.toList());
 
-        int score = 0;
-        for (int i = 0; i < scanBeforeDeadline; i++) {
-            score = score + unscannedBooks.get(i).score;
-        }
-        return score;
-    }
-
-    public List<Book> getBooks(int remainingDays, Map<Integer, Book> bookStore) {
-        return Collections.emptyList();
+        return unscannedBooks.subList(0, Math.min(scanBeforeDeadline, unscannedBooks.size()) - 1);
     }
 }
